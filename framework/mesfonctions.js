38a738bc1a3f127cfee656c3657d9fae0fd7f419
Rodrick <138665640+Hitsugaya76@users.noreply.github.com>
2024-01-07 11:15:19 +0100

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Djalega++
+/*_________by Ryuzaki
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'Zokou-MD',
+        pack: 'Ryuzaki-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
